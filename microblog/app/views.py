0b72d2bf5a76996124a8128eabe645ccce3d7284
Jonathan Guo <jonathang@synnex.com>
2015-03-16 21:46:32 +0800

    loops in templates

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -6,6 +6,17 @@ from app import app
 @app.route('/index')
 def index():
     user = {'nickname': 'tntC4stl3'}  # fake user
+    posts = [  # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
     return render_template('index.html',
                            title='Home',
-                           user=user)
+                           user=user,
+                           posts=posts)
