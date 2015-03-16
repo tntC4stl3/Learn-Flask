c7674b14f97c4e31732b2ae05e41c3a5fefe730c
Jonathan Guo <jonathang@synnex.com>
2015-03-16 21:39:10 +0800

    template to the rescue

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,7 +1,11 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'tntC4stl3'}  # fake user
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
